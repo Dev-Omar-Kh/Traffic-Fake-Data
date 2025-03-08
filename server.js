3919488104168f31b41c5da3b019cf864c8b85e2
unknown <omar.er3434@gmail.com>
2025-03-08 19:32:25 +0200

    first commit

--- server.js
+++ server.js
@@ -1,13 +1,12 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-
-const PORT = 8080;
 
 server.use(middlewares);
 server.use(router);
 
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`✅ JSON Server is running on port ${PORT}`);
+  console.log(`JSON Server is running on port ${PORT}`);
 });
